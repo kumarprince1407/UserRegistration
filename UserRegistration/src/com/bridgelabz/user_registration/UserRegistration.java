b36dd80b78f8663f188cd48a213ce250d694b97a
Prince Kumar <kumarprince1407@gmail.com>
2023-07-06 16:09:35 +0530

    Initial commit

---
+++ UserRegistration/src/com/bridgelabz/user_registration/UserRegistration.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.user_registration;
+
+public class UserRegistration {
+    public static void main(String[] args) {
+        System.out.println("Welcome to user registration program.");
+    }
+}
